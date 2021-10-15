43a559c917c515ec5bf177b37d3bbf311dcd18de
CarlDuFromChina <18556906294@163.com>
2021-10-15 15:16:52 +0800

    build

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Juejin Helper
+
+## 部署
+
+1、编译
+
+```
+$ yarn install
+$ yarn build
+```
+
+2、运行
+
+```
+node app
+```
+
+## 持久化
+
+推荐用`pm2`
--- src/log.ts
+++ src/log.ts
@@ -11,8 +11,35 @@ try {
 }
 
 var log4js = require('log4js');
-console.log(Object.keys(Object.prototype));
 
-var config = require('../log4js.json');
+var config = {
+  appenders: {
+    access: {
+      type: 'dateFile',
+      filename: 'log/access.log',
+      pattern: '-yyyy-MM-dd',
+      category: 'http',
+    },
+    app: {
+      type: 'file',
+      filename: 'log/app.log',
+      maxLogSize: 10485760,
+      numBackups: 3,
+    },
+    errorFile: {
+      type: 'file',
+      filename: 'log/errors.log',
+    },
+    errors: {
+      type: 'logLevelFilter',
+      level: 'ERROR',
+      appender: 'errorFile',
+    },
+  },
+  categories: {
+    default: { appenders: ['app', 'errors'], level: 'DEBUG' },
+    http: { appenders: ['access'], level: 'DEBUG' },
+  },
+};
 
 log4js.configure(config);
